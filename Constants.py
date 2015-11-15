11b56e39c579c7f458196b5f5051f74b7ee4b281
Keertan Kini <krkini16@mit.edu>
2015-11-15 17:29:51 -0500

    Changed Constants HOST to strong'

--- Constants.py
+++ Constants.py
@@ -1,8 +1,7 @@
 class Constants:
-    HOST = 18.189.57.110
+    HOST = '18.189.57.110'
     KEYWORDS = ['Clinton', 'clinton','Sanders','sanders','FeelTheBern','Trump', 'hillaryclinton','rubio', 'jeb','Jeb','Carson','carson','Fiorina','fiorina','Cruz','cruz','Jindall','jindall','Rand','rand']
     PORT = 8888
 
     def __init__():
         pass
-    
