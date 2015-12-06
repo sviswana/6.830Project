a485bba632dad803a4eb62e008491b795779c340
Srinidhi Viswanathan <srinidhi@mit.edu>
2015-12-05 20:04:13 -0500

    try-catch for query engine error

--- server.py
+++ server.py
@@ -46,30 +46,34 @@ def clientthread(conn):
         #Receiving from client
         data = conn.recv(1024)
         print data
-        params = qe.deserialize(data)
-        param_type = params[0]
-        print 'PARAMS', params
-        if param_type == QueryType.SELECT:
-            reply = db.select(params[1]/1000, params[2])
 
-        elif param_type == QueryType.INSERT:
-            if params[2]=='':
-                pass
+        try:
+            params = qe.deserialize(data)
+            param_type = params[0]
+            print 'PARAMS', params
+            if param_type == QueryType.SELECT:
+                reply = db.select(params[1]/1000, params[2])
+
+            elif param_type == QueryType.INSERT:
+                if params[2]=='':
+                    pass
+                else:
+                    db.insert(params[1]/1000,params[2], params[3])
+                    reply = "inserting " + data
+                
+                #reply = db.insert(params[1:])
+            elif param_type == QueryType.UPDATE:
+                reply = "updating"
+                #reply = db.update(params[1:])
             else:
-                db.insert(params[1]/1000,params[2], params[3])
-                reply = "inserting " + data
-            
-            #reply = db.insert(params[1:])
-        elif param_type == QueryType.UPDATE:
-            reply = "updating"
-            #reply = db.update(params[1:])
-        else:
-            # throw exception
-            reply = "Invalid arguments, should be start with SELECT, INSERT, or UPDATE"
-        print reply
-        conn.sendall(reply)
-
-    conn.close()
+                # throw exception
+                reply = "Invalid arguments, should be start with SELECT, INSERT, or UPDATE"
+            print reply
+            conn.sendall(reply)
+            conn.close()
+        except:
+            print 'Closing connection'
+            conn.close()
 
 #now keep talking with the client
 while 1:
